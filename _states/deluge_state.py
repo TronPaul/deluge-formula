38f5d6cbdc7d52783ee86628e0174f89d595624a
Mark McGuire <mark.b.mcg@gmail.com>
2015-11-26 20:51:21 -0600

    Revert "fix deluge munging"
    This reverts commit 2bf3fa72d3e31fd2e76126b5682c26fb19b3233b.

--- _states/deluge_state.py
+++ _states/deluge_state.py
@@ -18,23 +18,15 @@ def config_value(name, value, config_path):
            'changes': {},
            'result': True,
            'comment': ''}
-    value = _deluge_value(value)
+    # Deluge works with json values
+    json_value = json.dumps(value)
     cv = __salt__['deluge.get_config_value'](name, config_path)
-    if cv != value:
-        __salt__['deluge.set_config_value'](name, value, config_path)
-        ret['changes'][name] = {'old': cv, 'new': value}
-        ret['comment'] = '{} changed from {} to {}'.format(name, cv, value)
+    if cv != json_value:
+        __salt__['deluge.set_config_value'](name, json_value, config_path)
+        ret['changes'][name] = {'old': cv, 'new': json_value}
+        ret['comment'] = '{} changed from {} to {}'.format(name, cv, json_value)
         ret['result'] = True
     else:
-        ret['comment'] = '{} was already set to {}'.format(name, value)
+        ret['comment'] = '{} was already set to {}'.format(name, json_value)
         ret['result'] = True
     return ret
-
-def _deluge_value(value):
-    '''
-    Munge values into the format deluge uses
-    '''
-    if isinstance(value, bool):
-        return str(value).lower()
-    else:
-        return value
