cee47bd17c028501c5838135511571e9a074ffb8
Mark McGuire <mark.b.mcg@gmail.com>
2015-11-26 20:51:47 -0600

    Revert "use json values for comparison"
    This reverts commit 888a1fdf7d322f80876d5cc2dd4d537d6298f66e.

--- _states/deluge_state.py
+++ _states/deluge_state.py
@@ -1,8 +1,3 @@
-try:
-    import simplejson as json
-except ImportError:
-    import json
-
 __virtualname__ = 'deluge'
 
 
@@ -18,15 +13,13 @@ def config_value(name, value, config_path):
            'changes': {},
            'result': True,
            'comment': ''}
-    # Deluge works with json values
-    json_value = json.dumps(value)
     cv = __salt__['deluge.get_config_value'](name, config_path)
-    if cv != json_value:
-        __salt__['deluge.set_config_value'](name, json_value, config_path)
-        ret['changes'][name] = {'old': cv, 'new': json_value}
-        ret['comment'] = '{} changed from {} to {}'.format(name, cv, json_value)
+    if cv != value:
+        __salt__['deluge.set_config_value'](name, value, config_path)
+        ret['changes'][name] = {'old': cv, 'new': value}
+        ret['comment'] = '{} changed from {} to {}'.format(name, cv, value)
         ret['result'] = True
     else:
-        ret['comment'] = '{} was already set to {}'.format(name, json_value)
+        ret['comment'] = '{} was already set to {}'.format(name, value)
         ret['result'] = True
     return ret
